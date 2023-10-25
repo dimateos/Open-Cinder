b631be432103b02563c08ecd244c23b44c33d0bd
diego.mateos <diego.mateos@mecalux.com>
2023-10-25 15:11:04 +0200

    inline main macro

--- include/cinder/app/msw/AppMsw.h
+++ include/cinder/app/msw/AppMsw.h
@@ -108,6 +108,12 @@ void AppMsw::main( const RendererRef &defaultRenderer, const char *title, const 
 	AppBase::cleanupLaunch();
 }
 
+#define CINDER_APP_MSW_INLINE(APP, RENDERER, ... )                                                                  \
+{                                                                                                                   \
+	cinder::app::RendererRef renderer( new RENDERER );                                                              \
+    cinder::app::AppMsw::main<APP>( renderer, #APP, ##__VA_ARGS__ );                                                \
+}
+
 #define CINDER_APP_MSW( APP, RENDERER, ... )                                                                        \
 int __stdcall WinMain( HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, LPSTR /*lpCmdLine*/, int /*nCmdShow*/ )\
 {                                                                                                                   \
